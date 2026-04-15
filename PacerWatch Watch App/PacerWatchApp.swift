ed3db63dc03498da0f1e42b6231ff6d787f180af
joshwinston2230-pixel <joshwinston2230@gmail.com>
2026-04-15 21:36:52 +1000

    Initial Commit

---
+++ PacerWatch Watch App/PacerWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PacerWatchApp.swift
+//  PacerWatch Watch App
+//
+//  Created by Broski on 15/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct PacerWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
